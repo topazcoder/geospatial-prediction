49030db47bcd032a1895b01daba2df2b759c9611
topazcoder <christianrichardson.finance@gmail.com>
2024-12-23 10:53:32 -0600

    validator side fiber update

--- gaia/validator/validator.py
+++ gaia/validator/validator.py
@@ -11,7 +11,7 @@ from cryptography.fernet import Fernet
 import httpx
 from fiber.chain import chain_utils
 from fiber.logging_utils import get_logger
-from fiber.validator import client as vali_client
+from fiber.encrypted.validator import client as vali_client, handshake
 from fiber.chain.metagraph import Metagraph
 from substrateinterface import SubstrateInterface
 from gaia.tasks.defined_tasks.geomagnetic.geomagnetic_task import GeomagneticTask
@@ -107,8 +107,6 @@ class GaiaValidator:
             self.current_block = self.substrate.get_block()["header"]["number"]
             self.last_set_weights_block = self.current_block - 300
 
-            # print the entire set of environment variables
-            # logger.info(f"{os.environ}")
 
             return True
         except Exception as e:
@@ -147,40 +145,45 @@ class GaiaValidator:
                 base_url = f"https://{node.ip}:{node.port}"
 
                 try:
-                    payload_str = json.dumps(payload).encode()
-
-                    headers = vali_client.get_headers_with_nonce(
-                        payload_str=payload_str,
-                        validator_ss58_address=self.keypair.ss58_address,
-                        miner_ss58_address=miner_hotkey,
-                        keypair=self.keypair
+                    symmetric_key_str, symmetric_key_uuid = (
+                        await handshake.perform_handshake(
+                            keypair=self.keypair,
+                            httpx_client=self.httpx_client,
+                            server_address=base_url,
+                            miner_hotkey_ss58_address=miner_hotkey,
+                        )
                     )
 
-                    resp = await vali_client.make_non_streamed_post(
-                        httpx_client=self.httpx_client,
-                        server_address=base_url,
-                        validator_ss58_address=self.keypair.ss58_address,
-                        miner_ss58_address=miner_hotkey,
-                        keypair=self.keypair,
-                        endpoint=endpoint,
-                        payload=payload
-                    )
+                    if symmetric_key_str and symmetric_key_uuid:
+                        logger.info(f"Handshake successful with miner {miner_hotkey}")
+                        fernet = Fernet(symmetric_key_str)
 
-                    logger.debug(f"Raw response from miner {miner_hotkey}: {resp.text}")
-                    logger.debug(f"Response status code from miner {miner_hotkey}: {resp.status_code}")
+                        resp = await vali_client.make_non_streamed_post(
+                            httpx_client=self.httpx_client,
+                            server_address=base_url,
+                            fernet=fernet,
+                            keypair=self.keypair,
+                            symmetric_key_uuid=symmetric_key_uuid,
+                            validator_ss58_address=self.keypair.ss58_address,
+                            miner_ss58_address=miner_hotkey,
+                            payload=payload,
+                            endpoint=endpoint,
+                        )
 
-                    # resp.raise_for_status()
-                    # logger.debug(f"Response from miner {miner_hotkey}: {resp}")
-                    # logger.debug(f"Response text from miner {miner_hotkey}: {resp.headers}")
-                    # Create a dictionary with both response text and metadata
-                    response_data = {
-                        "text": resp.text,
-                        "hotkey": miner_hotkey,
-                        "port": node.port,
-                        "ip": node.ip,
-                    }
-                    responses[miner_hotkey] = response_data
-                    logger.info(f"Completed request to {miner_hotkey}")
+                        # resp.raise_for_status()
+                        # logger.debug(f"Response from miner {miner_hotkey}: {resp}")
+                        # logger.debug(f"Response text from miner {miner_hotkey}: {resp.headers}")
+                        # Create a dictionary with both response text and metadata
+                        response_data = {
+                            "text": resp.text,
+                            "hotkey": miner_hotkey,
+                            "port": node.port,
+                            "ip": node.ip,
+                        }
+                        responses[miner_hotkey] = response_data
+                        logger.info(f"Completed request to {miner_hotkey}")
+                    else:
+                        logger.warning(f"Failed handshake with miner {miner_hotkey}")
                 except httpx.HTTPStatusError as e:
                     logger.warning(f"HTTP error from miner {miner_hotkey}: {e}")
                     # logger.debug(f"Error details: {traceback.format_exc()}")
@@ -195,10 +198,6 @@ class GaiaValidator:
                     logger.error(f"Error with miner {miner_hotkey}: {e}")
                     logger.error(f"Error details: {traceback.format_exc()}")
                     continue
-
-            logger.debug(f"Raw responses received: {responses}")
-            for hotkey, response in responses.items():
-                logger.debug(f"Response from {hotkey}: {response['text']}")
 
             return responses
 
