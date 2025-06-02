06d23aa144726bf263defb210bce021433f926fa
topazcoder <christianrichardson.finance@gmail.com>
2025-06-02 14:54:32 +0000

    geomag, soil back on

--- gaia/validator/validator.py
+++ gaia/validator/validator.py
@@ -1544,8 +1544,8 @@ class GaiaValidator:
             logger.info("Auto-updater task started independently")
             
             tasks = [
-                #lambda: self.geomagnetic_task.validator_execute(self),
-                #lambda: self.soil_task.validator_execute(self),
+                lambda: self.geomagnetic_task.validator_execute(self),
+                lambda: self.soil_task.validator_execute(self),
                 lambda: self.weather_task.validator_execute(self),
                 lambda: self.status_logger(),
                 lambda: self.main_scoring(),
